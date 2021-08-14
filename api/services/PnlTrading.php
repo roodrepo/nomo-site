a8c7730cbdbd76f31d8332621175c2973c1a28a1
Rud f <rudyfernandez.pro@gmail.com>
2021-08-14 17:27:47 +0800

    fix trade table

--- api/services/PnlTrading.php
+++ api/services/PnlTrading.php
@@ -4,7 +4,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-if(isset($_POST['ignore']) and ($_POST['ignore'] === True or $_POST['ignore'] == 'True')){
+if(isset($_POST['ignore']) and ($_POST['ignore'] === True or strtolower($_POST['ignore']) == 'true')){
     echo json_encode(array('data' => array()));
     die();
 }
